e2e152d2323976867947bde21b6ba4762a9b5aa6
MasterUNG <phrombutr@gmail.com>
2017-12-14 11:10:48 +0700

    Add Fragment Success

--- app/src/main/java/androidthai/in/th/sstservice/MainActivity.java
+++ app/src/main/java/androidthai/in/th/sstservice/MainActivity.java
@@ -3,11 +3,27 @@ package androidthai.in.th.sstservice;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import androidthai.in.th.sstservice.fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+//        Add Fragment To Activity
+        if (savedInstanceState == null) {
+
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentMainFragment, new MainFragment())
+                    .commit();
+
+        }
+
+
+    }   // Main Method
+
+
+
+}   // Main Class
